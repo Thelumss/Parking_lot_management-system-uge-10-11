2bb054b13bf84ad8f5ea7955f55b2576a98956f2
Luke_sinclair <Lukesin@outlook.com>
2025-11-17 09:27:16 +0100

    Add project files.

---
+++ Parking_lot_management-system-uge-10-11/Program.cs
@@ -0,0 +1,36 @@
+
+namespace Parking_lot_management_system_uge_10_11
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
